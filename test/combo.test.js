f3476fd9881af3ce212c3374129b7b28ea10f5fd
iX315 <ix315@me.com>
2017-04-05 16:59:56 +0200

    add test for max arg

--- test/combo.test.js
+++ test/combo.test.js
@@ -25,5 +25,12 @@ suite('combo', function() {
 
     assert.deepEqual(out, [ [ 'a' ], [ 'b' ], [ 'c' ], [ 'a', 'b' ], [ 'a', 'c' ], [ 'b', 'c' ], [ 'a', 'b', 'c' ] ]);
   });
+
+  test('4 item array with min 2 and max 3 elements', function() {
+    var out = combo(['a', 'b', 'c', 'd'],2,3);
+
+    assert.deepEqual(out, [ [ 'a', 'b' ], [ 'a', 'c' ], [ 'a', 'd'], [ 'b', 'c' ], [ 'b', 'd'], [ 'c', 'd' ], [ 'a', 'b', 'c' ], [ 'a', 'b', 'd' ], [ 'a', 'c', 'd' ], [ 'b', 'c', 'd' ] ]);
+  });
+
 });
 
